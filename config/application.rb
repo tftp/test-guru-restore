301aa3446d7807a4dcfc065473fb5ac9cb6e8416
Oleg Goloborodko <goloborodko@inbox.ru>
2023-08-13 11:49:01 +0300

    add time zone and locale

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module TestGuru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.time_zone = 'Moscow'
+    config.i18n.default_locale = :ru
 
     # Configuration for the application, engines, and railties goes here.
     #
